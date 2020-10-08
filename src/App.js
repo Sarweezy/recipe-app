200bdb64e77518e69c5c2e5bed4759c3aab09b51
Sarwar Ahmed <sarwarahmed@Sarwars-MBP.verizon.net>
2020-10-08 16:34:44 -0400

    Added app ID and key from edamam recipe API as well as the get request URL

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+// Default text shown in React was the old version of React with class
+// components where now we use more functional components (with functions).
+
+const App = () => {
+
+  const APP_ID = "a79d8426";
+  const APP_KEY = "bb8e9ddaefa3eedd238a71a97ffca727";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
   );
 }
