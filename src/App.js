3a37ef3b05faac71324511432a51462c10a36b52
Sarwar Ahmed <sarwarahmed@Sarwars-MBP.verizon.net>
2020-10-08 18:54:13 -0400

    Minor additions of useEffect and useState functions as well as a counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 // Default text shown in React was the old version of React with class
@@ -9,13 +8,25 @@ const App = () => {
 
   const APP_ID = "a79d8426";
   const APP_KEY = "bb8e9ddaefa3eedd238a71a97ffca727";
-
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-    
+  
+  const [counter,setCounter] = useState(0);
+
+  useEffect(() =>{
+    console.log('Effect has been run');
+  });
 
   return(
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+
     </div>
   );
 }
